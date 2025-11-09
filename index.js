33a7a2b9ce4f3179e118a4d186166450004ad19c
riyadhossen74 <riyadhossen696969@gmail.com>
2025-11-09 16:47:25 +0600

    challenges data added

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://eco-track:3ubWeuL1ObeBTDGt@cluster0.cymbxlh.mongodb.net/?appName=Cluster0";
 
-
-
-const uri = "mongodb+srv://eco-track:3ubWeuL1ObeBTDGt@cluster0.cymbxlh.mongodb.net/?appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db("eco-track");
+    challengesCollection = db.collection("challenges");
+
+    app.get("/challenges", async (req, res) => {
+      const result = await challengesCollection.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,14 +41,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World! How are you')
-})
-app.get('/challenges', (req, res)=>{
-    res.send('challenges data')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World! How are you");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
