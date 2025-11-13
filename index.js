b02d9f4917cc0305c76ae9679312d41e58c43160
riyadhossen74 <riyadhossen696969@gmail.com>
2025-11-13 12:09:21 +0600

    added env file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion,  ObjectId } = require("mongodb");
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://eco-track:3ubWeuL1ObeBTDGt@cluster0.cymbxlh.mongodb.net/?appName=Cluster0";
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cymbxlh.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
